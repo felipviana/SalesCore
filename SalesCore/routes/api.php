5deca934e287953a446217aca594207b5c8b140d
Felps <felipe.viana0402@proton.me>
2026-06-04 19:17:19 -0300

    :test: Route test

---
+++ SalesCore/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/status', function () {
+    return response()->json([
+        'message' => 'API SalesCore funcionando',
+        'status' => 'ok'
+    ]);
+});
